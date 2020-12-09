51d6ce4d74ca3dd3e03e69f36509e2127aa9b8ab
Akshay <akshaybaweja.1996@gmail.com>
2020-12-09 12:39:33 +0530

    syntax

--- videofeed.py
+++ videofeed.py
@@ -54,10 +54,12 @@ class VideoFeed:
             if upperImageSliced.ndim is not 0 and lowerImageSliced.ndim is not 0:
                 if upperFaceArea < lowerFaceArea:
                     # Scale up upper face area
-                    cv2.resize(upperImageSliced, 0, 0, lowerFaceArea/upperFaceArea)
+                    d = lowerFaceArea/upperFaceArea
+                    cv2.resize(upperImageSliced, 0, 0, d, d)
                 elif lowerFaceArea < upperFaceArea:
                     # Scale up lower face area
-                    cv2.resize(lowerImageSliced, 0, 0, upperFaceArea/lowerFaceArea)
+                    d = upperFaceArea/lowerFaceArea
+                    cv2.resize(lowerImageSliced, 0, 0, d, d)
                 mergedImage = np.vstack((upperImageSliced, lowerImageSliced))
                 # print("Shukar hai rabba")
         elif upperImageSliced is not None and lowerImageSliced is None:
